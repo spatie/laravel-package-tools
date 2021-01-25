9a47c9a89b05a611f1d0c62dc3b62d3e28c9957a
freekmurze <freekmurze@users.noreply.github.com>
2021-01-25 14:41:59 +0000

    Fix styling

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -52,7 +52,7 @@ abstract class PackageServiceProvider extends ServiceProvider
             }
 
             foreach ($this->package->migrationFileNames as $migrationFileName) {
-                if (!$this->migrationFileExists($migrationFileName)) {
+                if (! $this->migrationFileExists($migrationFileName)) {
                     $this->publishes([
                         $this->package->basePath("/../database/migrations/{$migrationFileName}.php.stub") => database_path('migrations/' . now()->format('Y_m_d_His') . '_' . $migrationFileName),
                     ], "{$this->package->name}-migrations");
