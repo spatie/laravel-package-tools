b8ba66c0ba480b5bb779c970a18bfd54e4ce780b
Neil Carlo Sucuangco <succute@yahoo.com>
2022-08-17 17:29:00 +0800

    Update PackageServiceProviderTestCase.php

--- tests/PackageServiceProviderTests/PackageServiceProviderTestCase.php
+++ tests/PackageServiceProviderTests/PackageServiceProviderTestCase.php
@@ -3,6 +3,7 @@
 
 namespace Spatie\LaravelPackageTools\Tests\PackageServiceProviderTests;
 
+use Illuminate\Filesystem\Filesystem;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\Tests\TestCase;
 use Spatie\LaravelPackageTools\Tests\TestPackage\Src\ServiceProvider;
@@ -11,6 +12,14 @@ abstract class PackageServiceProviderTestCase extends TestCase
 {
     public function setUp(): void
     {
+        $this->afterApplicationCreated(function () {
+            (new Filesystem)->cleanDirectory(database_path('migrations'));
+        });
+
+        $this->beforeApplicationDestroyed(function () {
+            (new Filesystem)->cleanDirectory(database_path('migrations'));
+        });
+
         ServiceProvider::$configurePackageUsing = function (Package $package) {
             $this->configurePackage($package);
         };
