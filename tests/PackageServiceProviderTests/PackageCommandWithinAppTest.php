74a9423a650b6606320386c8ef1747c16b73b2e6
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-11 00:23:41 +0300

    refactor PackageCommandWithinAppTest

--- tests/PackageServiceProviderTests/PackageCommandWithinAppTest.php
+++ tests/PackageServiceProviderTests/PackageCommandWithinAppTest.php
@@ -5,8 +5,7 @@ namespace Spatie\LaravelPackageTools\Tests\PackageServiceProviderTests;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\Tests\TestClasses\TestCommand;
 
-class PackageCommandWithinAppTest extends PackageServiceProviderTestCase
-{
+trait ConfigurePackageCommandWithinAppTest {
     public function configurePackage(Package $package)
     {
         $package
@@ -14,12 +13,12 @@ class PackageCommandWithinAppTest extends PackageServiceProviderTestCase
             ->hasRoutes('web')
             ->hasCommand(TestCommand::class);
     }
-
-    /** @test */
-    public function it_can_execute_a_registered_command_in_the_context_of_the_app()
-    {
-        $response = $this->get('execute-command');
-
-        $response->assertSee('output of test command');
-    }
 }
+
+uses(ConfigurePackageCommandWithinAppTest::class);
+
+test('it can execute a registered command in the context of the app', function () {
+    $response = $this->get('execute-command');
+
+    $response->assertSee('output of test command');
+});
