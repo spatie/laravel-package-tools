b39dbd3946f22004c58e2659a73cf6619147e144
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-11 00:37:08 +0300

    refactor PackageTranslationsTest

--- tests/PackageServiceProviderTests/PackageTranslationsTest.php
+++ tests/PackageServiceProviderTests/PackageTranslationsTest.php
@@ -1,35 +1,32 @@
 <?php
 
-namespace Spatie\LaravelPackageTools\Tests\PackageServiceProviderTests;
-
 use Spatie\LaravelPackageTools\Package;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertFileExists;
 
-class PackageTranslationsTest extends PackageServiceProviderTestCase
-{
+trait ConfigurePackageTranslationsTest {
     public function configurePackage(Package $package)
     {
         $package
             ->name('laravel-package-tools')
             ->hasTranslations();
     }
-
-    /** @test */
-    public function it_can_load_the_translations()
-    {
-        $this->assertEquals('translation', trans('package-tools::translations.translatable'));
-    }
-
-    /** @test */
-    public function it_can_publish_the_translations()
-    {
-        $this
-            ->artisan('vendor:publish --tag=package-tools-translations')
-            ->assertExitCode(0);
-
-        $path = (function_exists('lang_path'))
-            ? lang_path("vendor/package-tools/en/translations.php")
-            : resource_path("lang/vendor/package-tools/en/translations.php");
-        
-        $this->assertFileExists($path);
-    }
 }
+
+uses(ConfigurePackageTranslationsTest::class);
+
+test('it can load the translations', function () {
+    assertEquals('translation', trans('package-tools::translations.translatable'));
+});
+
+test('it can publish the translations', function () {
+    $this
+        ->artisan('vendor:publish --tag=package-tools-translations')
+        ->assertExitCode(0);
+
+    $path = (function_exists('lang_path'))
+        ? lang_path("vendor/package-tools/en/translations.php")
+        : resource_path("lang/vendor/package-tools/en/translations.php");
+
+    assertFileExists($path);
+});
