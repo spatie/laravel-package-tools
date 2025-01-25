26ca62c6e19be9879089f7e1bc3e0f4102365eaa
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2025-01-25 21:56:33 +0000

    Update PackageServiceProviderTestCase.php

--- tests/PackageServiceProviderTests/PackageServiceProviderTestCase.php
+++ tests/PackageServiceProviderTests/PackageServiceProviderTestCase.php
@@ -4,6 +4,7 @@ namespace Spatie\LaravelPackageTools\Tests\PackageServiceProviderTests;
 
 use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\Tests\TestCase;
 use Spatie\LaravelPackageTools\Tests\TestPackage\Src\ServiceProvider;
@@ -12,6 +13,38 @@ use Symfony\Component\Finder\SplFileInfo;
 
 abstract class PackageServiceProviderTestCase extends TestCase
 {
+    protected array $cleanPaths = [
+        'app/Providers/',
+        'app/Livewire/',
+        'app/View/Components/vendor/',
+        'config/',
+        'database/migrations/',
+        'lang/vendor/',
+        'public/vendor/',
+        'resources/views/livewire/',
+        'resources/views/vendor/',
+        'resources/js/Pages/',
+        'storage/framework/views/',
+    ];
+
+    protected array $cleanExclusions = [
+        'config/app.php',
+        'config/auth.php',
+        'config/broadcasting.php',
+        'config/cache.php',
+        'config/concurrency.php',
+        'config/cors.php',
+        'config/database.php',
+        'config/filesystems.php',
+        'config/hashing.php',
+        'config/logging.php',
+        'config/mail.php',
+        'config/queue.php',
+        'config/services.php',
+        'config/session.php',
+        'config/view.php',
+    ];
+
     protected function setUp(): void
     {
         ServiceProvider::$configurePackageUsing = function (Package $package) {
@@ -45,27 +78,17 @@ abstract class PackageServiceProviderTestCase extends TestCase
 
     protected function deletePublishedFiles(): self
     {
-        $configPath = config_path('package-tools.php');
-
-        if (file_exists($configPath)) {
-            unlink($configPath);
-        }
-
-        $configPath = config_path('alternative-config.php');
-
-        if (file_exists($configPath)) {
-            unlink($configPath);
-        }
-
-        collect(File::allFiles(database_path('migrations')))
-            ->each(function (SplFileInfo $file) {
-                unlink($file->getPathname());
+        $basePath = base_path() . '/';
+        foreach ($this->cleanPaths as $dir) {
+            if (! is_dir($basePath . $dir)) {
+                continue;
+            }
+            collect(File::allFiles($basePath . $dir))->each(function (SplFileInfo $file) use ($basePath) {
+                if (!in_array(Str::replace('\\', '/', Str::after($file->getPathname(), $basePath)), $this->cleanExclusions)) {
+                    unlink($file->getPathname());
+                }
             });
-
-        collect(File::allFiles(app_path('Providers')))
-            ->each(function (SplFileInfo $file) {
-                unlink($file->getPathname());
-            });
+        }
 
         /* Clear publishes from previous tests */
         ServiceProvider::$publishes[ServiceProvider::class] = [];
