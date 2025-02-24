73e009b2799f1181b33b18588d85c8954125a838
Sophist-UK <3001893+Sophist-UK@users.noreply.github.com>
2025-02-24 23:12:00 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,11 +7,7 @@
 */
 
 
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use function PHPUnit\Framework\assertEmpty;
 use Spatie\LaravelPackageTools\Tests\PackageServiceProviderTests\PackageServiceProviderTestCase;
-use Symfony\Component\Finder\SplFileInfo;
 
 uses(PackageServiceProviderTestCase::class)->in('PackageServiceProviderTests');
 
